8ae7769c4eecb37b91ed015cf57dcf9d073205f2
barisgcynr@gmail.com <barisgcynr@gmail.com>
2021-12-23 13:03:12 +0300

    Update

---
+++ src/main/java/com/comp301project/Entity/TestObj.java
@@ -0,0 +1,15 @@
+package com.comp301project.Entity;
+
+import java.util.List;
+
+public class TestObj {
+
+    private List<Machine> machineList;
+    private String hourStr;
+
+}
+
+class Machine {
+    private String machineName;
+    private boolean reserved;
+}
